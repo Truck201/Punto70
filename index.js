3e63020eb094ffd535fef79032f0efe44bbdaa0f
Truck201 <joaquingandini22@gmail.com>
2024-11-26 13:06:45 -0700

    changes fetch

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.use((req, res) => {
   res.status(404).send("<h1>404</h1>");
 });
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 8080;
 
 app.listen(PORT, () => {
   console.log(`server listening in http://localhost:${PORT}`);
